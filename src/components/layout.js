621a6c72dcd0f69125e8e1705688d97785cf73ad
Fatos Idrizi <fatos.idrizi@gmail.com>
2021-12-12 15:05:59 +0100

    adding css

---
+++ src/components/layout.js
@@ -0,0 +1,28 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import {
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText
+} from './layout.module.css'
+
+const Layout = ({ pageTitle, pageHeading, children, navLinkItem, navLinkText }) => {
+    return (
+        <main className={container}>
+            <title>{pageTitle}</title>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}><Link to="/contact" className={navLinkText}>Contact</Link></li>
+                </ul>
+            </nav>
+            <h1 className={heading}>{pageHeading}</h1>
+            {children}
+        </main>
+    )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
-import { Link } from "gatsby"
 import * as React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 
 
 // markup
 const IndexPage = () => {
   return (
-    <Layout>
-      <title>Home Page</title>
+    <Layout pageTitle='Home Page' pageHeading='Home'>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
